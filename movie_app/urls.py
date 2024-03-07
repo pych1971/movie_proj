883ab56cf334216227ecc06b40fb9ec14119d1ed
pych1971 <pych1971@gmail.com>
2024-03-07 14:42:37 +0500

    2024-03-07-14-42

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.show_all_movie),
+    path('movie/<int:id_movie>', views.show_one_movie, name='movie-detail'),
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
@@ -8,4 +8,11 @@ def show_all_movie(request):
     movies = Movie.objects.all()
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies
+    })
+
+
+def show_one_movie(request, id_movie: int):
+    movie = get_object_or_404(Movie, id=id_movie)
+    return render(request, 'movie_app/one_movie.html', {
+        'movie': movie
     })
