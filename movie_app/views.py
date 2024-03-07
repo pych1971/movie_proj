89c30c1f05a0bea5c65d8a10d8fae96ffe389fa6
pych1971 <pych1971@gmail.com>
2024-03-07 14:08:13 +0500

    2024-03-07-14-08

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Movie
+
 
 # Create your views here.
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_app/all_movies.html', {
+        'movies': movies
+    })
