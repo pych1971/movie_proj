96fdcbff110b0f89c01ca912c1ed535f91273ae9
pych1971 <pych1971@gmail.com>
2024-03-10 17:02:55 +0500

    2024-03-10-17-02

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Movie
 
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget']
+    list_editable = ['rating', 'year', 'budget']
+    ordering = ['-rating', '-name']
+    list_per_page = 5
+
 # Register your models here.
-admin.site.register(Movie)
