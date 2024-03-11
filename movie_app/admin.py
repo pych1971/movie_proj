408c582f3c9075e21a115793416dfe46369c77e5
pych1971 <pych1971@gmail.com>
2024-03-11 10:22:27 +0500

    2024-03-11-10-22

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -4,9 +4,17 @@ from .models import Movie
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget']
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
     list_editable = ['rating', 'year', 'budget']
     ordering = ['-rating', '-name']
-    list_per_page = 5
+    list_per_page = 10
 
-# Register your models here.
+    @admin.display(ordering='rating', description='Статус')
+    def rating_status(self, mov: Movie):
+        if mov.rating < 50:
+            return 'Зачем это смотреть?!'
+        if mov.rating < 70:
+            return 'Разок можно глянуть'
+        if mov.rating <= 85:
+            return 'Зачёт'
+        return 'Топчик'
