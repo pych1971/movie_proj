53fd4deffa0ee723b65c83275f002ee0f1b2eeee
pych1971 <pych1971@gmail.com>
2024-03-11 10:57:23 +0500

    2024-03-11-10-57

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
+from django.db.models import QuerySet
 
 
 @admin.register(Movie)
@@ -8,6 +9,7 @@ class MovieAdmin(admin.ModelAdmin):
     list_editable = ['rating', 'currency', 'budget']
     ordering = ['-rating', '-name']
     list_per_page = 10
+    actions = ['set_dollars', 'set_euro']
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, mov: Movie):
@@ -18,3 +20,16 @@ class MovieAdmin(admin.ModelAdmin):
         if mov.rating <= 85:
             return 'Зачёт'
         return 'Топчик'
+
+    @admin.action(description='Установить валюту в доллар')
+    def set_dollars(self, request, qs: QuerySet):
+        qs.update(currency=Movie.USD)
+
+    @admin.action(description='Установить валюту в евро')
+    def set_euro(self, request, qs: QuerySet):
+        count_updated = qs.update(currency=Movie.EUR)
+        self.message_user(
+            request,
+            f'Было обновлено {count_updated} записей',
+            messages.ERROR
+        )
