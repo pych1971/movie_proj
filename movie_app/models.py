3dee2691f065b27b0137e90394202044613d9eee
pych1971 <pych1971@gmail.com>
2024-03-12 10:30:27 +0500

    2024-03-12-10-30

--- movie_app/models.py
+++ movie_app/models.py
@@ -4,7 +4,14 @@ from django.utils.text import slugify
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    director_email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Movie(models.Model):
     EUR = 'EUR'
